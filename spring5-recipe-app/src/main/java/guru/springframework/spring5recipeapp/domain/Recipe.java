ab5bb91bca41a48503a637d58ce16a7839b1aa9c
sjcalabrese <erotavlas103@gmail.com>
2019-08-21 11:59:12 -0400

    Added Ingredient and built relationship to recipe.

--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -20,6 +21,9 @@ public class Recipe {
     private String directions;
     //todo add
     //private Difficulty difficulty
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     //enters as a blob.
     //@lob = large object
@@ -107,5 +111,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
