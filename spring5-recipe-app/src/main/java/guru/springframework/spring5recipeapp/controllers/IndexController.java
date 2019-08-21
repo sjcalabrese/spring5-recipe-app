5e792f04eff4e36be433d1d778f40e6fe8184658
sjcalabrese <erotavlas103@gmail.com>
2019-08-21 15:46:08 -0400

    added query methods to categoryreop and uon repo.

--- spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,15 +1,33 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import guru.springframework.spring5recipeapp.domain.Category;
+import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
+import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage()
     {
-        System.out.println("Auto Restart");
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUom("Teaspoon");
+
+        System.out.println("Category id is " + categoryOptional.get().getId());
+        System.out.println("Uom id is " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
