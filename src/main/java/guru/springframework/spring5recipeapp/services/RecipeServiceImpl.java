20e8273ee5ceca75038f830644d204804bfb0463
sjcalabrese <erotavlas103@gmail.com>
2019-09-19 11:50:37 -0400

    added show recipe page and related controller/services

--- src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -3,9 +3,11 @@ package guru.springframework.spring5recipeapp.services;
 import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.apache.bcel.classfile.annotation.RuntimeInvisTypeAnnos;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -24,5 +26,17 @@ public class RecipeServiceImpl implements RecipeService {
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long l){
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+
+        if(!recipeOptional.isPresent()){
+            throw new RuntimeException("Recipe Not Found!");
+        }
+
+        return recipeOptional.get();
     }
 }
