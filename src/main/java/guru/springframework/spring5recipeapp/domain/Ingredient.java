53ace96f6163a98033fe723ec2f90f302f57dc33
Sal Calabrese <erotavlas103@gmail.com>
2023-02-22 16:19:22 -0500

    Updating @Data with @getter and @Setter for Ingredient

--- src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -5,7 +5,9 @@ import lombok.*;
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Data
+//@Data is causing issues
+@Getter
+@Setter
 @EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
